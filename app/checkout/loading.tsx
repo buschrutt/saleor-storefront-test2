65248528c1715332de8ad97e988a333412a02dd7
Vladimir Nagornov <buschrutt@gmail.com>
2026-01-01 12:06:28 -0600

    Saleor Replased -bugs

--- app/checkout/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <span className="text-3xl font-bold tracking-wide">Loading Checkout 555 ...</span>
-        </div>
-    );
-}
